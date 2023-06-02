c653503573847ea6c583664247139445c3156a3c
Darshan Soni <100061941+drsnsoni@users.noreply.github.com>
2023-06-02 11:46:15 +0530

    Laravel CRUD API
    This tasks Done In this Commit
    -Install A new Laravel app
    -Create first route
    -model resource
    -resource route
    -creating resource
    -update a resource
    -delete a resource

---
+++ task-manager/app/Http/Controllers/TaskController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpadteTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+    public function index(Request $request){
+        return new TaskCollection(Task::all());
+    }
+
+    public function show(Request $request, Task $task){
+        return new TaskResource($task);
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $validated = $request->validated();
+        $task = Task::create($validated);
+        return new TaskResource($task);
+    }
+
+    public function update(UpadteTaskRequest $request, Task $task)
+    {
+        $validated =  $request->validated();
+        $task->update($validated);
+        return new TaskResource($task);
+    }
+
+    public function destroy(Request $request, Task $task)
+    {
+
+        $task->delete();
+        return response()->noContent();
+    }
+}
---
+++ task-manager/app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'is_done'];
+
+    protected $cast = [
+        'is_done' => 'boolean'
+    ];
+
+    protected $hidden = [
+        'updated_at'
+    ];
+}
